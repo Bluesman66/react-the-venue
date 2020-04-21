f33ef65b7133ff951d670562b311185fcf6a0b15
alex.paclin <alex.paclin@gmail.com>
2020-04-21 13:51:12 +0500

    021 Adding a Scroll To

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './resources/styles.css';
 
 import { Footer, Header } from './components/header-footer';
 
+import { Element } from 'react-scroll';
 import { Featured } from './components/featured';
 import { Highlights } from './components/highlights';
 import { Location } from './components/location';
@@ -13,11 +14,21 @@ const App = () => {
 	return (
 		<div style={{ height: '1500px', background: 'cornflowerblue' }}>
 			<Header />
-			<Featured />
-			<VenueNfo />
-			<Highlights />
-			<Pricing />
-			<Location />
+			<Element name="featured">
+				<Featured />
+			</Element>
+			<Element name="venuenfo">
+				<VenueNfo />
+			</Element>
+			<Element name="highlights">
+				<Highlights />
+			</Element>
+			<Element name="pricing">
+				<Pricing />
+			</Element>
+			<Element name="location">
+				<Location />
+			</Element>
 			<Footer />
 		</div>
 	);
--- src/components/venue-nfo/VenueNfo.js
+++ src/components/venue-nfo/VenueNfo.js
@@ -3,22 +3,23 @@ import Zoom from 'react-reveal/Zoom';
 import icon_calendar from '../../resources/images/icons/calendar.png';
 import icon_location from '../../resources/images/icons/location.png';
 
-const vnItem = (icon, bkg, title, desc) => {
-	return (
-		<div className="vn_item">
-			<div className="vn_outer">
-				<div className="vn_inner">
-					<div className="vn_icon_square" style={{ background: `${bkg}` }}></div>
-					<div className="vn_icon" style={{ background: `url(${icon})` }}></div>
-					<div className="vn_title">{title}</div>
-					<div className="vn_desc">{desc}</div>
+const VenueNfo = () => {
+
+	const vnItem = (icon, bkg, title, desc) => {
+		return (
+			<div className="vn_item">
+				<div className="vn_outer">
+					<div className="vn_inner">
+						<div className="vn_icon_square" style={{ background: `${bkg}` }}></div>
+						<div className="vn_icon" style={{ background: `url(${icon})` }}></div>
+						<div className="vn_title">{title}</div>
+						<div className="vn_desc">{desc}</div>
+					</div>
 				</div>
 			</div>
-		</div>
-	);
-};
+		);
+	};
 
-const VenueNfo = () => {
 	return (
 		<div className="bck_black">
 			<div className="center_wrapper">
